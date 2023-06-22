6419aaeac6f50af6d5a1aee8c223e0e8d1474a0e
guntekhunter <guntek2000@gmail.com>
2023-06-22 20:33:29 +0800

    fix: stream output

--- src/app/peringkas/ContainerPeringkas.tsx
+++ src/app/peringkas/ContainerPeringkas.tsx
@@ -12,19 +12,20 @@ export default function ContainerPeringkas() {
   const [questions, setQuestions] = useState<{ chat: any }[]>([]);
   const [isDrop, setIsDrop] = useState(false);
   const [selected, setSelected] = useState("");
+  const [sentenceLength, setSentenceLength] = useState("");
   const [isSelected, setIsSelected] = useState(false);
   const [summary, setSummary] = useState("");
   const [isCopied, setIsCopied] = useState(false);
-  const arrayPersentage = [
-    "20%",
-    "30%",
-    "40%",
-    "50%",
-    "60%",
-    "70%",
-    "80%",
-    "90%",
-  ];
+  // const arrayPersentage = [
+  //   "20",
+  //   "30",
+  //   "40",
+  //   "50",
+  //   "60",
+  //   "70",
+  //   "80",
+  //   "90",
+  // ];
 
   const startResume = async () => {
     const props = `Create a summary of the following sentence, ${input} making it ${selected} shorter than the original sentence. in indonesian`;
@@ -38,24 +39,56 @@ export default function ContainerPeringkas() {
           model: "gpt-3.5-turbo",
           messages: [
             {
-              role: "user",
-              content: props,
+              role: "system",
+              content: `You are a language model that can generate summaries. make it in ${sentenceLength} sentences`,
             },
+            {
+              role: "user",
+              content: input,
+            },
+            // {
+            //   role: "assistant",
+            //   content: `/summarize\nlength:10`,
+            // },
           ],
+          stream: true,
         }),
       });
 
-      const chat = await res.json();
-      const chatData = chat.choices[0].message.content;
-      console.log(chatData);
-      setSummary(chatData);
+      const reader = res.body?.getReader();
+      const decoder = new TextDecoder("utf-8");
+      let completeSummary = "";
+
+      while (true) {
+        if (reader) {
+          const chunk = await reader.read();
+          const { done, value } = chunk;
+          if (done) {
+            break;
+          }
+          const decodedChunk = decoder.decode(value);
+          const lines = decodedChunk.split("\n");
+          const parsedLines = lines
+            .map((line) => line.replace(/^data: /, "").trim())
+            .filter((line) => line !== "" && line !== "[DONE]")
+            .map((line) => JSON.parse(line));
+
+          for (const parsedLine of parsedLines) {
+            const { choices } = parsedLine;
+            const { delta } = choices[0];
+            const { content } = delta;
+
+            if (content) {
+              completeSummary += content;
+              console.log(content);
+            }
+          }
+          setSummary(completeSummary);
+        }
+      }
     } catch (error) {
       console.log(error);
     }
-  };
-
-  const hanldeDropDown = () => {
-    setIsDrop(!isDrop);
   };
 
   const handleCopy = () => {
@@ -73,6 +106,7 @@ export default function ContainerPeringkas() {
     <div className="w-full flex justify-around">
       <div className="w-[70%] inline py-[2rem]">
         <textarea
+          placeholder="Masukkan kalimat yang ingin anda ringkas"
           className="w-full border-[1.5px] border-gray-200 rounded-md appearance-none h-[15rem] overflow-y-scroll border-t-[1px] px-5 scrollbar-thin scrollbar-track-[#F5F8FA] scrollbar-thumb-black resize-none focus:ring-0 focus:outline-none py-[1rem] mb-[1.1rem]"
           name=""
           id=""
@@ -88,48 +122,15 @@ export default function ContainerPeringkas() {
         />
         <div className="flex space-x-5 text-[1rem]">
           <div className="w-full relative space-y-[.4rem]">
-            <button
-              onClick={hanldeDropDown}
+            <input
+              placeholder="Masukkan jumlah kalimat"
+              type="number"
+              onChange={(e) => {
+                setSentenceLength(e.target.value);
+              }}
+              value={sentenceLength}
               className="w-full border-[1.5px] rounded-md flex px-[1rem] py-[1rem] space-x-[.7rem]"
-            >
-              <Image
-                alt="turtles"
-                src="/down.png"
-                width={500}
-                height={0}
-                className={`w-[1.5rem] transform duration-200 ${
-                  isDrop ? "rotate-180" : ""
-                }`}
-              ></Image>
-              <p className={`${isSelected ? "" : "text-gray-400"}`}>
-                {isSelected ? selected : "Pilih Panjang ringkasan"}
-              </p>
-            </button>
-            <div
-              className={`transfrom duration-100 ease-in z-10 ${
-                isDrop ? "" : "opacity-0 "
-              } absolute bg-white border-[1.5px] rounded-md w-full right-0 h-[5rem] overflow-y-scroll border-t-[1px] scrollbar-thin scrollbar-track-[#F5F8FA] scrollbar-thumb-black focus:ring-0 focus:outline-none`}
-            >
-              <div className="">
-                {arrayPersentage.map((item, key) => (
-                  <div
-                    key={key}
-                    onClick={(e) => {
-                      setSelected(item);
-                      setIsSelected(true);
-                      setIsDrop(false);
-                    }}
-                    className={`${
-                      key === arrayPersentage.length - 1
-                        ? ""
-                        : "border-b-[1.5px]"
-                    } py-[.5rem] px-[1rem] cursor-pointer trasnform duration-200 hover:bg-black hover:text-white w-full`}
-                  >
-                    {item}
-                  </div>
-                ))}
-              </div>
-            </div>
+            />
           </div>
           <button
             onClick={startResume}
@@ -139,12 +140,12 @@ export default function ContainerPeringkas() {
           </button>
         </div>
         <div className="relative z-0">
-          <textarea
-            name=""
+          <div
             id=""
-            value={summary}
             className="w-full border-[1.5px] border-gray-200 rounded-md appearance-none h-[15rem] overflow-y-scroll border-t-[1px] px-5 scrollbar-thin scrollbar-track-[#F5F8FA] scrollbar-thumb-black resize-none focus:ring-0 focus:outline-none py-[1rem] mt-[1.4rem]"
-          />
+          >
+            {summary}
+          </div>
           <button className="absolute z-1 right-5 top-8" onClick={handleCopy}>
             <Image
               alt="turtles"

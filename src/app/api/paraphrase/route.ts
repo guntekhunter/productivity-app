4020c33ab6bbac39623709c3576fb945a47b815c
guntekhunter <guntek2000@gmail.com>
2023-07-09 19:54:19 +0800

    fead: update the api key

--- src/app/api/paraphrase/route.ts
+++ src/app/api/paraphrase/route.ts
@@ -7,7 +7,7 @@ export const POST = async (req: Request, res: Response) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.PARAPHRASE_API_KEY}`,
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
       },
       body: JSON.stringify(reqBody),
     });

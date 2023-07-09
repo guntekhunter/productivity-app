cbfb75d483c912eeb9d4b733087301a4111cb497
guntekhunter <guntek2000@gmail.com>
2023-07-09 15:35:02 +0800

    fix: the google auth

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXTAUTH_SECRET: "https://productivity-8d37mcwjr-guntekhunter.vercel.app/",
+    NEXTAUTH_SECRET: "https://productivity-app-six.vercel.app/",
   },
 };
 
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,9 +4,12 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: `${process.env.GOOGLE_ID}`,
-      clientSecret: `${process.env.GOOGLE_SECRET}`,
+      clientId:
+        `${process.env.GOOGLE_ID}` ??
+        "358240887503-i6ca4q6og1plib64clcrjr7u5qt0nva2.apps.googleusercontent.com",
+      clientSecret:
+        `${process.env.GOOGLE_SECRET}` ?? "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",
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.SECRET ?? "962ccba5a239f611ac57e5a718b60918",
 });

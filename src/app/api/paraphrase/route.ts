026da91d95e412eb121f8d8b2ec6d594d80ab610
guntekhunter <guntek2000@gmail.com>
2023-07-11 07:40:32 +0800

    fix: edge

--- src/app/api/paraphrase/route.ts
+++ src/app/api/paraphrase/route.ts
@@ -1,13 +1,7 @@
 const DATA_SOURCE_URL = "https://api.openai.com/v1/chat/completions";
 
 export const config = {
-  runtime: "edge", // for Edge API Routes only
-  unstable_allowDynamic: [
-    // allows a single file
-    "/lib/utilities.js",
-    // use a glob to allow anything in the function-bind 3rd party module
-    "/node_modules/function-bind/**",
-  ],
+  runtime: "experimental-edge",
 };
 
 export const POST = async (req: Request, res: Response) => {

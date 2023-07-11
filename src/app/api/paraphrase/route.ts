dc41663aa831f43580c42b8b470cee1719c4886c
guntekhunter <guntek2000@gmail.com>
2023-07-11 08:16:04 +0800

    fead: edge

--- src/app/api/paraphrase/route.ts
+++ src/app/api/paraphrase/route.ts
@@ -1,7 +1,7 @@
 const DATA_SOURCE_URL = "https://api.openai.com/v1/chat/completions";
 
 export const config = {
-  runtime: "edge",
+  runtime: "experimental-edge",
 };
 
 export default async function handler(req: Request, res: Response) {
